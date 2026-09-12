fcbe730301fc6334a43dee8f4d93d75900b4d72d
tiagosilvagba <tiagosilvagba@gmail.com>
2026-09-12 12:45:29 -0300

    Atualiza cache para tema Palmeiras Neon

--- sw-data-cache.js
+++ sw-data-cache.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const CSV_CACHE_NAME = "almoxarifado-csv-v5";
-const APP_CACHE_NAME = "almoxarifado-app-v5";
+const CSV_CACHE_NAME = "almoxarifado-csv-v6";
+const APP_CACHE_NAME = "almoxarifado-app-v6";
 const CSV_PATTERN = /\.csv(?:$|\?)/i;
 const SCRIPT_PATTERN = /\/script\.js$/i;
 const STYLE_PATTERN = /\/style\.css$/i;
