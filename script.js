37c09a766845f127401f574c878e099af32689e6
tiagosilvagba <tiagosilvagba@gmail.com>
2026-09-14 11:58:36 -0300

    Comparativo: atualizar Excel detalhado e versão 2.8

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const PREVIOUS_BOOTSTRAP_FALLBACK = "https://raw.githubusercontent.com/tiagosilv
 const COMPARATIVO_META_MODULE = "./comparativo-meta.js?v=20260914-1";
 const LAYOUT_FIX_MODULE = "./layout-fix.js?v=20260914-2";
 const COMPARATIVO_PERIODOS_MODULE = "./comparativo-periodos.js?v=20260914-1";
-const COMPARATIVO_EXCEL_MODULE = "./comparativo-excel-padrao.js?v=20260914-2";
+const COMPARATIVO_EXCEL_MODULE = "./comparativo-excel-padrao.js?v=20260914-3";
 const COMPARATIVO_ITEM_SPLIT_MODULE = "./comparativo-item-split.js?v=20260914-2";
 const COMPARATIVO_META_VISUAL_MODULE = "./comparativo-meta-visual.js?v=20260914-2";
 const COMPARATIVO_INCLUSOES_MODULE = "./comparativo-inclusoes.js?v=20260914-1";
 const COMPARATIVO_CARDS_RESUMO_MODULE = "./comparativo-cards-resumo.js?v=20260914-1";
 const COMPARATIVO_MES_A_MES_MODULE = "./comparativo-mes-a-mes.js?v=20260914-1";
-const CURRENT_PUBLIC_VERSION = "Versão 2.7";
+const CURRENT_PUBLIC_VERSION = "Versão 2.8";
 
 function loadIncrementalScript(src) {
   return new Promise((resolve, reject) => {
