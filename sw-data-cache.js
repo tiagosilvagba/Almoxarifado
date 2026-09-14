d972ca22eeb4cbd1f7a082332be6dff08d07640a
tiagosilvagba <tiagosilvagba@gmail.com>
2026-09-14 09:18:57 -0300

    Atualiza cache da aplicacao para restauracao do menu desktop

--- sw-data-cache.js
+++ sw-data-cache.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const CSV_CACHE_NAME = "almoxarifado-csv-v10";
-const APP_CACHE_NAME = "almoxarifado-app-v10";
+const CSV_CACHE_NAME = "almoxarifado-csv-v11";
+const APP_CACHE_NAME = "almoxarifado-app-v11";
 const CSV_PATTERN = /\.csv(?:$|\?)/i;
 const SCRIPT_PATTERN = /\/script\.js$/i;
 const STYLE_PATTERN = /\/style\.css$/i;
