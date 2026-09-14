f3a3af2f7028625d382b6a62ee02ddf3c31b1454
tiagosilvagba <tiagosilvagba@gmail.com>
2026-09-14 12:04:40 -0300

    Atualiza cache para comparativo Meta vs mês comparado

--- sw-data-cache.js
+++ sw-data-cache.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const CSV_CACHE_NAME = "almoxarifado-csv-v26";
-const APP_CACHE_NAME = "almoxarifado-app-v26";
+const CSV_CACHE_NAME = "almoxarifado-csv-v27";
+const APP_CACHE_NAME = "almoxarifado-app-v27";
 const CSV_PATTERN = /\.csv(?:$|\?)/i;
 const SCRIPT_PATTERN = /\/script\.js$/i;
 const STYLE_PATTERN = /\/style\.css$/i;
