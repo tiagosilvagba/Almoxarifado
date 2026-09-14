5af7828ad43a88aab07e84f3b910ac7352847c21
tiagosilvagba <tiagosilvagba@gmail.com>
2026-09-14 11:42:44 -0300

    Atualiza versão e recarrega exportação ordenada

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const PREVIOUS_BOOTSTRAP_FALLBACK = "https://raw.githubusercontent.com/tiagosilv
 const COMPARATIVO_META_MODULE = "./comparativo-meta.js?v=20260914-1";
 const LAYOUT_FIX_MODULE = "./layout-fix.js?v=20260914-2";
 const COMPARATIVO_PERIODOS_MODULE = "./comparativo-periodos.js?v=20260914-1";
-const COMPARATIVO_EXCEL_MODULE = "./comparativo-excel-padrao.js?v=20260914-1";
+const COMPARATIVO_EXCEL_MODULE = "./comparativo-excel-padrao.js?v=20260914-2";
 const COMPARATIVO_ITEM_SPLIT_MODULE = "./comparativo-item-split.js?v=20260914-2";
 const COMPARATIVO_META_VISUAL_MODULE = "./comparativo-meta-visual.js?v=20260914-1";
 const COMPARATIVO_INCLUSOES_MODULE = "./comparativo-inclusoes.js?v=20260914-1";
 const COMPARATIVO_CARDS_RESUMO_MODULE = "./comparativo-cards-resumo.js?v=20260914-1";
-const CURRENT_PUBLIC_VERSION = "Versão 2.4";
+const CURRENT_PUBLIC_VERSION = "Versão 2.5";
 
 function loadIncrementalScript(src) {
   return new Promise((resolve, reject) => {
