aff7eff2cb911e776b00f605e7e93e671390638c
tiagosilvagba <tiagosilvagba@gmail.com>
2026-09-14 12:21:16 -0300

    Atualiza cache após correção de formatação do comparativo

--- sw-data-cache.js
+++ sw-data-cache.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const CSV_CACHE_NAME = "almoxarifado-csv-v27";
-const APP_CACHE_NAME = "almoxarifado-app-v27";
+const CSV_CACHE_NAME = "almoxarifado-csv-v28";
+const APP_CACHE_NAME = "almoxarifado-app-v28";
 const CSV_PATTERN = /\.csv(?:$|\?)/i;
 const SCRIPT_PATTERN = /\/script\.js$/i;
 const STYLE_PATTERN = /\/style\.css$/i;
