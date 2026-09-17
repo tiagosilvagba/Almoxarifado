e7b6fe989d2ec5331c56ffa43f60562d0eae128f
tiagosilvagba <tiagosilvagba@gmail.com>
2026-09-17 20:35:55 -0300

    Aplica contraste estrutural global v9.2

--- app/modules/responsive-layout.js
+++ app/modules/responsive-layout.js
@@ -24,7 +24,7 @@
         const link = document.createElement("link");
         link.id = "iosProjectStyles";
         link.rel = "stylesheet";
-        link.href = "./app/styles/ios.css?v=9.0";
+        link.href = "./app/styles/ios.css?v=9.2";
         document.head.appendChild(link);
       }
     } else if (existing) {
