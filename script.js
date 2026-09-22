e36dc1379ebc2f1f5f669a5716d6fbacf1d8cbbf
tiagosilvagba <tiagosilvagba@gmail.com>
2026-09-22 17:30:38 -0300

    Corrigir largura da escala de 80% no desktop

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-/** Bootstrap 9.8: unifica a versão exibida desde o primeiro quadro da página. */
-const BOOTSTRAP_VERSION = "9.8";
+/** Bootstrap 9.9: unifica a versão exibida desde o primeiro quadro da página. */
+const BOOTSTRAP_VERSION = "9.9";
 const BOOTSTRAP_VERSION_LABEL = `Versão ${BOOTSTRAP_VERSION}`;
 window.__ALMOX_VERSION__ = BOOTSTRAP_VERSION;
 window.__ALMOX_VERSION_LABEL__ = BOOTSTRAP_VERSION_LABEL;
